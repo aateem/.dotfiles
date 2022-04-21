307c062ee430cc318d2a38c7e3fe6dbedf164c00
Artem Roma <artem.roma@vgs.io>
2022-04-21 22:53:42 +0300

    Try wezterm

---
+++ config/.config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+
+return {
+    color_scheme = "nord",
+    font = wezterm.font("Iosevka Nerd Font Mono", { weight = "Regular" }),
+    font_size = 16.0,
+    audible_bell = "Disabled",
+    keys = {
+        -- override tab motion (similar to Iterm2 defaults)
+        { key = "LeftArrow", mods = "SUPER", action = wezterm.action { ActivateTabRelative = -1 } },
+        { key = "RightArrow", mods = "SUPER", action = wezterm.action { ActivateTabRelative = 1 } },
+
+        -- trying to utilize vim movement shceme
+        { key = "H", mods = "CTRL|SHIFT", action = wezterm.action { ActivatePaneDirection = "Left" } },
+        { key = "L", mods = "CTRL|SHIFT", action = wezterm.action { ActivatePaneDirection = "Right" } },
+        { key = "K", mods = "CTRL|SHIFT", action = wezterm.action { ActivatePaneDirection = "Up" } },
+        { key = "J", mods = "CTRL|SHIFT", action = wezterm.action { ActivatePaneDirection = "Down" } },
+
+        { key = "Q", mods = "CTRL|SHIFT", action = wezterm.action { CloseCurrentPane = { confirm = true } } }
+    },
+}
