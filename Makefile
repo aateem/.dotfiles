1202bc16672df551c872cfac5c354e799b1a67e5
Artem Roma <fuzzy.finder+github@gmail.com>
2023-08-01 19:01:30 +0300

    Add the makefile
    Trying to speed up the setup as I keep forgetting the particualarities

---
+++ Makefile
@@ -0,0 +1,24 @@
+config_dir = $HOME/.config
+
+brew:
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+base: brew
+	brew install \
+		git \
+		stow \
+		fish
+	mkdir $config_dir
+
+tools:
+	brew install starship
+
+nerd-fonts:
+	brew tap homebrew/cask-fonts && \
+		brew install --cask font-jetbrains-mono-nerd-font
+
+shell: base tools nerd-fonts
+	stow terminal-emulators -t $config_dir
+	stow prompts -t $config_dir
+
+
--- terminal-emulators/wezterm/wezterm.lua
+++ terminal-emulators/wezterm/wezterm.lua
@@ -1,67 +1,77 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
-local cached_fonts = {
+local fonts = {
     iosevka_term = wezterm.font("IosevkaTerm Nerd Font", { weight = "Regular" }),
     iosevka = wezterm.font("Iosevka Nerd Font", { weight = "Regular" }),
     source_code_pro = wezterm.font("SauceCodePro Nerd Font", { weight = "Regular" }),
+    jetbrains = wezterm.font "JetBrainsMono Nerd Font", 
 }
 
 local cached_colors = {
     n = "nord",
+    b = "Batman",
     trf = "terafox",
     hlk = "The Hulk",
-    tkyo_m = "tokyonight_moon",
+    tokyo_m = "tokyonight_moon",
     tmrw_bl = "Tomorrow Night Blue",
     tmrw_brn = "Tomorrow Night Burns",
     tch = "ToyChest",
+    at = "AdventureTime",
+}
+local colorize = cached_colors.tokyo_m
+
+config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+
+config.font_size = 16.0
+config.font = fonts.jetbrains
+
+config.color_scheme = colorize
+config.default_cwd = os.getenv("HOME")
+config.audible_bell = "Disabled"
+config.use_dead_keys = false
+config.keys = {
+-- override tab motion (similar to Iterm2 defaults)
+	{
+	    key = "LeftArrow",
+	    mods = "SUPER",
+	    action = wezterm.action({ ActivateTabRelative = -1 }),
+	},
+	{
+	    key = "RightArrow",
+	    mods = "SUPER",
+	    action = wezterm.action({ ActivateTabRelative = 1 }),
+	},
+
+	-- trying to utilize vim movement shceme
+	{
+	    key = "H",
+	    mods = "CTRL|SHIFT",
+	    action = wezterm.action({ ActivatePaneDirection = "Left" }),
+	},
+	{
+	    key = "L",
+	    mods = "CTRL|SHIFT",
+	    action = wezterm.action({ ActivatePaneDirection = "Right" }),
+	},
+	{
+	    key = "K",
+	    mods = "CTRL|SHIFT",
+	    action = wezterm.action({ ActivatePaneDirection = "Up" }),
+	},
+	{
+	    key = "J",
+	    mods = "CTRL|SHIFT",
+	    action = wezterm.action({ ActivatePaneDirection = "Down" }),
+	},
+
+	{
+	    key = "Q",
+	    mods = "CTRL|SHIFT",
+	    action = wezterm.action({ CloseCurrentPane = { confirm = true } }),
+	},
 }
 
-return {
-    color_scheme = cached_colors.trf,
-    default_cwd = os.getenv("HOME"),
-    font = cached_fonts.iosevka_term,
-    font_size = 16.0,
-    audible_bell = "Disabled",
-    use_dead_keys = false,
-    keys = {
-        -- override tab motion (similar to Iterm2 defaults)
-        {
-            key = "LeftArrow",
-            mods = "SUPER",
-            action = wezterm.action({ ActivateTabRelative = -1 }),
-        },
-        {
-            key = "RightArrow",
-            mods = "SUPER",
-            action = wezterm.action({ ActivateTabRelative = 1 }),
-        },
-
-        -- trying to utilize vim movement shceme
-        {
-            key = "H",
-            mods = "CTRL|SHIFT",
-            action = wezterm.action({ ActivatePaneDirection = "Left" }),
-        },
-        {
-            key = "L",
-            mods = "CTRL|SHIFT",
-            action = wezterm.action({ ActivatePaneDirection = "Right" }),
-        },
-        {
-            key = "K",
-            mods = "CTRL|SHIFT",
-            action = wezterm.action({ ActivatePaneDirection = "Up" }),
-        },
-        {
-            key = "J",
-            mods = "CTRL|SHIFT",
-            action = wezterm.action({ ActivatePaneDirection = "Down" }),
-        },
-
-        {
-            key = "Q",
-            mods = "CTRL|SHIFT",
-            action = wezterm.action({ CloseCurrentPane = { confirm = true } }),
-        },
-    },
-}
+return config
